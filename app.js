83bc495de12937336a82cf738b460c80351d8b32
nino kristesiashvili <kristesiashvilinino@gmail.com>
2024-12-27 14:18:42 +0400

    remove unnecessary whitespace and clean up server response message

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const http = require('http');
 http.createServer(function(req,res){
-
 	res.write("WOLF BITCHING BIIITCH");
 	res.end();
-
-
 }
 ).listen(3000);
 
 console.log("WOLV biiitCHing on port 3000");
-
