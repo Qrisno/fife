50123be7f8b57d7d39139792cf4cd840d59eb0ec
qrisno <kristesiashvilinino@gmail.com>
2024-12-27 10:04:49 +0000

    first

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+http.createServer(function(req,res){
+
+	res.write("WOLF BITCHING BIIITCH");
+	res.end();
+
+
+}
+).listen(3000);
+
+console.log("WOLV biiitCHing on port 3000");
+
